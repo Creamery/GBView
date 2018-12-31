1eca0534744e65fc7c43bfaedbbd981d7c03d6f3
Candy <candy.espulgar@gmail.com>
2019-01-01 05:45:45 +0900

    Semi icons

--- app/src/main/java/seebee/geebeeview/layout/ViewPatientActivity.java
+++ app/src/main/java/seebee/geebeeview/layout/ViewPatientActivity.java
@@ -347,7 +347,8 @@ public class ViewPatientActivity extends AppCompatActivity {
                 (Button) findViewById(R.id.btn_about_sidebar),
                 (Button) findViewById(R.id.btn_hpi_sidebar),
                 (Button) findViewById(R.id.btn_immunization_sidebar),
-                (ConstraintLayout) findViewById(R.id.cont_sidebar_blank_hide));
+                (ConstraintLayout) findViewById(R.id.cont_sidebar_blank_hide),
+                (Button) findViewById(R.id.btn_back));
 
         sidebarManager.setItemsSidebarExtend(new ArrayList<ConstraintLayout>());
         sidebarManager.getItemsSidebarExtend().add((ConstraintLayout)findViewById(R.id.sidebar_extend_body_bg_hide));
@@ -355,6 +356,13 @@ public class ViewPatientActivity extends AppCompatActivity {
         sidebarManager.getItemsSidebarExtend().add((ConstraintLayout)findViewById(R.id.cont_hpi_extend_hide));
         sidebarManager.getItemsSidebarExtend().add((ConstraintLayout)findViewById(R.id.cont_immunization_extend_hide));
         sidebarManager.getItemsSidebarExtend().add(sidebarManager.getContSidebarBlank());
+
+        this.sidebarManager.getBtnSidebarBack().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         this.sidebarManager.getContSidebarBlank().setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/seebee/geebeeview/sidebar/PatientSidebar.java
+++ app/src/main/java/seebee/geebeeview/sidebar/PatientSidebar.java
@@ -7,16 +7,25 @@ import android.widget.ImageView;
 import java.util.ArrayList;
 
 public class PatientSidebar extends SidebarParent {
-    private Button btnSidebarAbout, btnSidebarHPI, btnSidebarImmunization;
+    private Button btnSidebarAbout, btnSidebarHPI, btnSidebarImmunization, btnSidebarBack;
     private ConstraintLayout contSidebarBlank;
     private ArrayList<ConstraintLayout> itemsSidebarExtend;
 
-    public PatientSidebar(Button btnOpen, Button btnExtend, Button btnAbout, Button btnHPI, Button btnImmunization, ConstraintLayout contBlank) {
+    public PatientSidebar(Button btnOpen, Button btnExtend, Button btnAbout, Button btnHPI, Button btnImmunization, ConstraintLayout contBlank, Button btnBack) {
         super(btnOpen, btnExtend);
         this.setBtnSidebarAbout(btnAbout);
         this.setBtnSidebarHPI(btnHPI);
         this.setBtnSidebarImmunization(btnImmunization);
         this.setContSidebarBlank(contBlank);
+        this.setBtnSidebarBack(btnBack);
+    }
+
+    public Button getBtnSidebarBack() {
+        return btnSidebarBack;
+    }
+
+    public void setBtnSidebarBack(Button btnSidebarBack) {
+        this.btnSidebarBack = btnSidebarBack;
     }
 
     public ConstraintLayout getContSidebarBlank() {
